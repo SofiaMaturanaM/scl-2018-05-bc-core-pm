5d5910b09788a0647c7725f558ea653f3a5799ba
sofimatsu@gmail.com <v.galleani.vg@gmail.com>
2018-05-31 15:46:37 -0400

    fórmula de función para cifrar arreglada (?)

--- src/cipher.js
+++ src/cipher.js
@@ -2,31 +2,31 @@ window.cipher = {
 
     encode: (string, offset) => {
         // para obtener el string 
-        let string = document.getElementById("encodeButton");
+        string = document.getElementById("boxText");
         // para obtener el offset
-        let offset = parseInt.document.getElementById("numberOffset");
+        offset = parseInt(document.getElementById("numberOffset"));
         //variable para retornar el mensaje
         let cipherText = "";
         // loop para codificar el mensaje
         for (i = 0; i < string.lenght; i++) {
 
             // para ubicar en valor ASCCI los caracteres ingresados
-            let ingresedText = string.toCharCodeAt(i);
+            let ingresedText = string.CharCodeAt(i);
 
             // condicional para codificar caracteres en mayúscula
             if (ingresedText >= 65 && ingresedText <= 90) {
-                cipherText += string.fromCharCode(ingresedText - 65 + offset)
+                cipherText += string.fromCharCode((ingresedText - 65 + offset) % 26 + 65)
 
                 // condicional para caracteres en minúscula   
             } else if (ingresedText >= 97 && ingresedText <= 122) {
-                ciphertext += string.fromCharCode(ingresedText - 97 + offset)
+                ciphertext += string.fromCharCode((ingresedText - 97 + offset) % 26 + 97)
 
                 // condicional para considerar los espacios
             } else(ingresedText == 32); {
-                ciphertext += string.fromCharCode(ingresedText - 32 + offset)
+                ciphertext += string.fromCharCode((ingresedText - 32 + offset) % 26 + 32)
 
             }
-            return cipherText
+            return console.log(cipherText)
         }
     }
 
