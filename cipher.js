858b6b9c8c70d881886f48b9a597c1277bd367ce
sofimatsu@gmail.com <v.galleani.vg@gmail.com>
2018-05-31 15:28:47 -0400

    función de cifrado hecha

--- cipher.js
+++ cipher.js
@@ -7,16 +7,29 @@ window.cipher = {
         let offset = parseInt.document.getElementById("numberOffset");
         //variable para retornar el mensaje
         let cipherText = "";
-
         // loop para codificar el mensaje
         for (i = 0; i < string.lenght; i++) {
+
             // para ubicar en valor ASCCI los caracteres ingresados
             let ingresedText = string.toCharCodeAt(i);
 
-            if (ingresedText > 65)
+            // condicional para codificar caracteres en mayúscula
+            if (ingresedText >= 65 && ingresedText <= 90) {
+                cipherText += string.fromCharCode(ingresedText - 65 + offset)
 
+                // condicional para caracteres en minúscula   
+            } else if (ingresedText >= 97 && ingresedText <= 122) {
+                ciphertext += string.fromCharCode(ingresedText - 97 + offset)
+
+                // condicional para considerar los espacios
+            } else(ingresedText == 32); {
+                ciphertext += string.fromCharCode(ingresedText - 32 + offset)
+
+            }
+            return cipherText
         }
     }
+
 
     ,
 
