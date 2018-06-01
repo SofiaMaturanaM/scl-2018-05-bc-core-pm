1907f21b77f0247edbef190668d7086b5c4c04c0
SofiaMaturanaM <sofimatsu@gmail.com>
2018-06-01 15:30:44 -0400

    proyecto

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 // const abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 // let cipherEncodeButton = document.getElementById("decodeButton");
+let string = document.getElementById("boxTextencode");
+let string2 = document.getElementById("textdecipher");
+// para obtener el offset
+let offset = document.getElementById("numberOffset");
+
+document.getElementById('encodeButton').addEventListener('click', function() {
+    document.getElementById('textdecipher').innerHTML = window.cipher.encode(string.value, parseInt(offset.value));
+
+});
+
+
+
+document.getElementById('decodeButton').addEventListener('click', function() {
+    document.getElementById("textdecipher").innerHTML = window.cipher.decode(string2.value, parseInt(offset.value))
+});
