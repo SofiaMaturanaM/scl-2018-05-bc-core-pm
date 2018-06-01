857f27db0da7e21c5541bf91f945b40675007514
SofiaMaturanaM <sofimatsu@gmail.com>
2018-06-01 15:30:22 -0400

    cifrado

--- cipher.js
+++ cipher.js
@@ -2,44 +2,43 @@ window.cipher = {
 
     encode: (string, offset) => {
         // para obtener el string 
-        string = document.getElementById("boxText");
-        // para obtener el offset
-        offset = parseInt(document.getElementById("numberOffset"));
+
         //variable para retornar el mensaje
         let cipherText = "";
+
         // loop para codificar el mensaje
-        for (i = 0; i < string.lenght; i++) {
+        for (let i = 0; i < string.length; i++) {
 
-            // para ubicar en valor ASCCI los caracteres ingresados
-            // let ingresedText = string.CharCodeAt(i);
+            string = string.toUpperCase();
 
-            // condicional para codificar caracteres en mayúscula
-            if (ingresedText >= 65 && ingresedText <= 90) {
-                cipherText += string.fromCharCode((ingresedText - 65 + offset) % 26 + 65)
+            let toASCCI = string.charCodeAt(i);
 
-                // condicional para caracteres en minúscula   
-                // } else if (ingresedText >= 97 && ingresedText <= 122) {
-                //     ciphertext += string.fromCharCode((ingresedText - 97 + offset) % 26 + 97)
+            let position = (toASCCI - 65 + offset) % 26 + 65;
 
-                // condicional para considerar los espacios
-                // } else(ingresedText == 32); {
-                //     ciphertext += string.fromCharCode((ingresedText - 32 + offset) % 26 + 32)
-
-                // }
-            }
-            return console.log(cipherText)
-
+            let fromASCCI = String.fromCharCode(position);
+            cipherText += fromASCCI;
         }
+        return cipherText;
 
+    },
 
-        // ,
+    decode: (string, offset) => {
 
-        decode: (string, offset) => {
-            string = document.getElementById("decodeButton");
-            offset = parseInt.document.getElementById("numberOffset");
+        let decipherText = "";
 
-            // usar fromCharCode() para decodificar!!!!
+        for (let j = 0; j < string.length; j++) {
+
+            string = string.toUpperCase();
+
+            let toASCCI = string.charCodeAt(j);
+
+            let position = (toASCCI + 65 - offset) % 26 + 65;
+
+            let fromASCCI = String.fromCharCode(position);
+            decipherText += fromASCCI;
         }
+        return decipherText;
+
+
     }
-
 }
